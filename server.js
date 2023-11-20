81c6e527254593c37ac3b643b2242bf4444c8c6e
Joel Gale <144267824+WestColtv@users.noreply.github.com>
2023-11-20 10:30:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 const app = require("./index")
 
 const port = process.env.port || 3000;
 
 app.listen(port, ()=>{
     console.log('Servidor en linea...');
-=======
-const app = require("./index")
 
-const port = process.env.port || 3000;
-
-app.listen(port, ()=>{
-    console.log('Servidor en linea...');
->>>>>>> a6c4cc47b96054c983047fa4842cc72ee89bdbf7
 })
