55afac8a11ab0072268a9e942a9760d50b60dc87
Joel Gale <144267824+WestColtv@users.noreply.github.com>
2023-11-20 10:51:15 -0500

    Update cliente.router.js

--- router/cliente.router.js
+++ router/cliente.router.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const router = require("express").Router()
 const controller = require('../controllers/clientes.control')
 
@@ -22,29 +22,4 @@ router.delete('/clientes',function(req,res){
     res.render('index');   
 });
 
-=======
-const router = require("express").Router()
-const controller = require('../controllers/clientes.control')
-
-router.get('/clientes',controller.clientes)
-router.get('/index',controller.clientes)
-router.get('/segunda',controller.clientes)
-router.get('/tercera',controller.clientes)
-router.get('/iniciarsesion',controller.clientes)
-router.get('/formulario',controller.clientes)
-
-
-router.post('/clientes',function(req,res){    
-    res.render('index');   
-});
-
-router.put('/clientes',function(req,res){    
-    res.render('index');   
-});
-
-router.delete('/clientes',function(req,res){    
-    res.render('index');   
-});
-
->>>>>>> a6c4cc47b96054c983047fa4842cc72ee89bdbf7
 module.exports = router;
