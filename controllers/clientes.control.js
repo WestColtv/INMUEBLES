fc83c55c0b5bd69deff5ee09aae435b3ccff659a
Joel Gale <144267824+WestColtv@users.noreply.github.com>
2023-11-20 10:42:02 -0500

    Update clientes.control.js

--- controllers/clientes.control.js
+++ controllers/clientes.control.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const controller =  {}
 
 controller.clientes = function(req,res){    
@@ -21,28 +21,4 @@ controller.clientes = function(req,res){
     res.render('formulario');
 }
 
-=======
-const controller =  {}
-
-controller.clientes = function(req,res){    
-    res.render('index');
-}
-
-controller.clientes = function(req,res){    
-    res.render('segunda');
-}
-
-controller.clientes = function(req,res){    
-    res.render('tercera');
-}
-
-controller.clientes = function(req,res){    
-    res.render('iniciarsesion');
-}
-
-controller.clientes = function(req,res){    
-    res.render('formulario');
-}
-
->>>>>>> a6c4cc47b96054c983047fa4842cc72ee89bdbf7
 module.exports = controller
