48bcb09ce0b5b87aa7a7dcc39ebc9c6162547c73
Joel Gale <144267824+WestColtv@users.noreply.github.com>
2023-11-20 10:47:10 -0500

    Update script.js

--- publico/css/script.js
+++ publico/css/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let loginForm = document.querySelector(".my-form");
 
 loginForm.addEventListener("submit", (e) => {
@@ -8,17 +8,4 @@ loginForm.addEventListener("submit", (e) => {
 
     console.log('Email:', email.value);
     console.log('Password:', password.value);
-    // process and send to API 
-=======
-let loginForm = document.querySelector(".my-form");
-
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
-
-    console.log('Email:', email.value);
-    console.log('Password:', password.value);
-    // process and send to API 
->>>>>>> a6c4cc47b96054c983047fa4842cc72ee89bdbf7
 });
